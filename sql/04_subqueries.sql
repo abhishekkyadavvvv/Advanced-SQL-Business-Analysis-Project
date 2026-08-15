189efbb947ca5d5404612c14bb074afa95798c04
Abhishek Yadav <abhi0007yadav@gmail.com>
2026-08-15 13:53:39 +0530

    Update 04_subqueries.sql

--- sql/04_subqueries.sql
+++ sql/04_subqueries.sql
@@ -1,7 +1,14 @@
 -- Q1. Find customers whose total spending is greater than the average customer spending.
-
+with xyz as (select c.customer_name,sum(o.amount) as total_spend from customeers.c join orders o
+  on c.customer_id = o.customer_id
+  group by c.customer_name)
+select customer_name,total_spend from xyz
+where total_spend>(select avg(total_spend) from xyz)
+  
 -- Q2. Find employees whose salary is greater than the average employee salary.
-
+select employee_name , salary from employees 
+where salary>(select avg(salary) from employees)
+  
 -- Q3. Find products whose price is greater than the average product price.
 
 -- Q4. Find the customer who placed the highest-value order.
